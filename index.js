ccf59aec2bad479f0fbcbef10ab147bd8aa8b9aa
Balthier <rrodrijose@gmail.com>
2022-06-13 19:32:44 -0500

    final

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-function newImage(url, left, bottom){
+function newImage(url, leftSide, bottomSide){
     let object = document.createElement('img')
-    object.src = URL
+    object.src = url
     object.style.position = 'fixed'
-    object.style.left = left + 'px'
-    object.style.bottom = bottom +'px'
+    object.style.left = leftSide + 'px'
+    object.style.bottom = bottomSide +'px'
     document.body.append(object)
     return object
 }
 
-function newItem(url, left, bottom){
-    let object = newImage(url, left, bottom)
+function newItem(url, leftSide, bottomSide){
+    let object = newImage(url, leftSide, bottomSide)
+
     object.addEventListener('dblclick', () => {
-        object.remove
-    })
+        object.remove()
+    })/*my eventlistener isint functioning when double clicking on thew items*/
 }
 newImage('assets/green-character.gif', 100, 100)
 newImage('assets/pine-tree.png', 450, 200)
@@ -21,5 +22,6 @@ newImage('assets/pillar.png', 350, 100)
 newImage('assets/crate.png', 150, 200)
 newImage('assets/well.png', 500, 425)
 newImage('assets/sword.png', 500, 405)
-newImage("assets/shield.png", 165, 185)
+newImage("assets/sheild.png", 165, 185)/*shield is misspelled 
+will cause it to break */
 newImage('assets/staff.png', 600, 100)
